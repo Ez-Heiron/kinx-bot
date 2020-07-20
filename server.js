eea78b9f0c95430734cbcda73251776c184b0364
Ez-Heiron <68299262+Ez-Heiron@users.noreply.github.com>
2020-07-20 22:49:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login(process.env.BOT_TOKEN);;
-
 client.on('ready', () => {
 console.log("Conectado como " + client.user.tag )
 
-client.user.setActivity("Mi Prefix Es / Pero estoy siendo configurado :) ", {type: "playing"})})
+client.on('message', msg => {
+  if (msg.author == client.user){return}
+  if (msg.author.id == "159985870458322944"){return}
+  let message = msg.content.toLowerCase()
+  if(message.includes("/info")) {
+    msg.reply('He sido creado por Heiron y estoy encantado de estar en todos los servidores para divertirme :D')
+  }});
+
+client.login(process.env.BOT_TOKEN);
